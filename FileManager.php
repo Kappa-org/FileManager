01c366f1adaa919f4e44cbcc64f79ff89b596e89
Ondřej Záruba <zarubaondra@gmail.com>
2012-09-30 15:56:19 +0200

    Refactoring to inject*()

--- FileManager.php
+++ FileManager.php
@@ -7,20 +7,40 @@
 
 class FileManagerPresenter extends \Nette\Application\UI\Presenter
 {
+	/**
+	 * @var Kappa\Packages\FileManager\FileManagerFactory
+	 */
+	private $fileManagerFactory;
+
 	public function startup()
 	{
 		parent::startup();
 		$this->setLayout(False);
 	}
 
+	/**
+	 * @param Kappa\Packages\FileManager\FileManagerFactory $fileManagerFactory
+	 */
+	public function injectFileManagerFactory(\Kappa\Packages\FileManager\FileManagerFactory $fileManagerFactory)
+	{
+		$this->fileManagerFactory = $fileManagerFactory;
+	}
+
+	/**
+	 * @return array
+	 *
+	 */
 	public function formatTemplateFiles()
 	{
 		return array(LIBS_DIR . '/FileManager/Templates/default.latte');
 	}
 
+	/**
+	 * @return Kappa\Packages\FileManager\FileManagerControl
+	 */
 	protected function createComponentManager()
 	{
-		$manager = new \Kappa\Packages\FileManager\FileManagerFactory;
+		$manager = $this->fileManagerFactory;
 		return $manager->create();
 	}
 }
