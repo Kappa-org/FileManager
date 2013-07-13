ee39295c2735d9d450f7cd78b20262066b74ac91
Budry <zarubaondra@gmail.com>
2013-07-13 19:46:12 +0200

    Removed dependency set layout in presenter

--- src/Kappa/FileManager/FileManagerControl.php
+++ src/Kappa/FileManager/FileManagerControl.php
@@ -153,6 +153,7 @@ class FileManagerControl extends Control
 	public function render()
 	{
 		$this->template->setFile(__DIR__ . '/Templates/default.latte');
+		$this->template->assetsDir = $this->_params['assetsDir'];
 		$this->template->navigation = $this->session->actualDir;
 		$this->template->directories = $this->getActualDir()->getDirectories();
 		$this->template->files = $this->getActualDir()->getFiles();
