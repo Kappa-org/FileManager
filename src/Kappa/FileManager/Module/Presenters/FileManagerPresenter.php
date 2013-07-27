487749e3f5d4d85010773257ea441a9901c4c761
Budry <zarubaondra@gmail.com>
2013-07-27 20:40:24 +0200

    Added module for work with filemanager

---
+++ src/Kappa/FileManager/Module/Presenters/FileManagerPresenter.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the filemanager package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\FileManager\Module\Presenters;
+
+use Nette\Application\UI\Presenter;
+
+class FileManagerPresenter extends Presenter
+{
+	/**
+	 * @inject
+	 * @var \Kappa\FileManager\FileManagerFactory
+	 */
+	public $fileManagerFactory;
+
+	protected function beforeRender()
+	{
+		$this->template->assetsDir = $this->fileManagerFactory->getAssetsDir();
+	}
+
+	protected function createComponentFileManager()
+	{
+		$manager = $this->fileManagerFactory;
+		$manager->setType($this->getParameter('type'));
+		return $manager->create();
+	}
+}
