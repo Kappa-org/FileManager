430fa01cba28fca2341de60d3c0161c5b43da390
Budry <zarubaondra@gmail.com>
2013-10-05 12:30:59 +0200

    Added exception

---
+++ src/Kappa/FileManager/exceptions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the Kappa\FileManager package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\FileManager;
+
+/**
+ * Class InvalidStateException
+ * @package Kappa\FileManager
+ */
+class InvalidStateException extends \RuntimeException
+{
+
+}
